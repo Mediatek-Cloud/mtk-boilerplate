e8fb37f26b93ddf7a9a5fbe6c8ea5aea8fbdc5a8
CT Wu (吳敬庭) <CT.Wu@mediatek.com>
2015-04-23 17:31:49 +0800

    modified wepack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var PathRewriterPlugin = require('webpack-path-rewriter');
-
-var cssExtractor = new ExtractTextPlugin('css', 'main.css');
-var htmlExtractor = new ExtractTextPlugin('html', 'index.html');
 
 module.exports = {
   context: path.resolve('./src'),
@@ -16,21 +11,15 @@ module.exports = {
   },
   module: {
     loaders: [
-
-      // { test: /\.html?$/, loader: PathRewriterPlugin.rewriteAndEmit({ name: 'index.html' })},
-      { test: /\.html?$/, loader: htmlExtractor.extract('html-loader')},
+      { test: /\.html?$/, loader: 'html-loader'},
       { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      { test: /\.styl$/, loader: cssExtractor.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers: ["> 1%"]}!stylus-loader') },
+      { test: /\.styl$/, loaders: ['style-loader', 'css-loader', 'autoprefixer-loader?{browsers: ["> 1%"]}', 'stylus-loader'] },
       { test: /\.otf$/, loaders: ['file-loader?name=[name].[ext]'] },
       { test: /\.svg$/, loaders: ['url-loader?limit=10000&name=[name].[ext]'] }
     ]
   },
   plugins: [
-    new webpack.NoErrorsPlugin(),
-    cssExtractor,
-    htmlExtractor
-
-    // new PathRewriterPlugin()
+    new webpack.NoErrorsPlugin()
   ],
   devtool: 'eval-source-map'
 };
